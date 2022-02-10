5c43533913bab17e248995d7c738eb49bd5d18e2
Sergen <yukselsergen1@gmail.com>
2022-02-10 07:58:43 +0100

    change config for mac mamp

--- competences_gps-test/src/v/identification.php
+++ competences_gps-test/src/v/identification.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=competences;charset=utf8', 'root', 'password', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=localhost;dbname=competences;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch (Exeption $e)
 {
